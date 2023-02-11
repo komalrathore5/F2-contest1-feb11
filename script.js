552f517760da71434b00b3d6f9f9bc2c815885ad
komalrathore5 <komalkanwarrathore2014@gmail.com>
2023-02-11 19:25:50 +0530

    function

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(obj =>{
+    if(obj.marks >= 50){
+      console.log(obj);
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(element =>{
+    if(element.marks >=50){
+      console.log(element);
+    }
+  })  
 }
 
 function addData() {
   //Write your code here, just console.log
-}
+  let obj={id:4,name:"susan",age:"20",marks:45};
+  arr.push(obj);
+  console.log(arr);
+  }
+
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i =0;i<=arr.length;i++)
+  {
+    if(arr[i].marks <= 50){
+      arr.splice(i,i+1);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let ids = arr.length;
+  let arr1 = [
+    { id: ids + 1, name: "jony", age: "19", marks: 85 },
+    { id: ids + 2, name: "jackson", age: "18", marks: 80 },
+    { id: ids + 3, name: "krishna", age: "20", marks: 45 },
+  ];
+  let array = arr.concat(arr1);
+  console.log(array);
 }
+
